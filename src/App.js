3298ce9c1fe1595bf1b026df3a0b1fdd5b33aade
Felipe Talavera <ft.redprince@gmail.com>
2023-03-27 15:27:50 -0300

    add logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+    Button,
+    Container,
+    Box,
+    Typography,
+    List,
+    ListItem,
+    ListItemText, styled,
+} from '@mui/material';
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const StyledBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+  max-width: 400px;
+`;
 
 function App() {
+  const [tasks, setTasks] = useState(['Tarea 1', 'Tarea 2']);
+
+  const handleButtonClick = () => {
+    const newTask = `Tarea ${tasks.length + 1}`;
+    setTasks([...tasks, newTask]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <StyledContainer>
+        <Typography variant="h4" gutterBottom>
+          Lista de tareas
+        </Typography>
+        <StyledBox>
+          <List>
+            {tasks.map((task, index) => (
+                <ListItem key={index}>
+                  <ListItemText primary={task} />
+                </ListItem>
+            ))}
+          </List>
+          <Button variant="contained" color="primary" onClick={handleButtonClick}>
+            Añadir tarea
+          </Button>
+        </StyledBox>
+      </StyledContainer>
   );
 }
 
